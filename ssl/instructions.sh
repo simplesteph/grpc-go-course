eb1eaf5c2469903cfdd923209c5e66c33baf4ad3
Said Saifi <said.saifi@ubanquity.com>
2020-12-05 20:38:53 +0200

    Update Steps 3 and 4 to add SAN to both server.csr and server.crt #15

--- ssl/instructions.sh
+++ ssl/instructions.sh
@@ -24,10 +24,10 @@ openssl req -passin pass:1111 -new -x509 -days 3650 -key ca.key -out ca.crt -sub
 openssl genrsa -passout pass:1111 -des3 -out server.key 4096
 
 # Step 3: Get a certificate signing request from the CA (server.csr)
-openssl req -passin pass:1111 -new -key server.key -out server.csr -subj "/CN=${SERVER_CN}"
+openssl req -passin pass:1111 -new -key server.key -out server.csr -subj "/CN=${SERVER_CN}" -config ssl.cnf
 
 # Step 4: Sign the certificate with the CA we created (it's called self signing) - server.crt
-openssl x509 -req -passin pass:1111 -days 3650 -in server.csr -CA ca.crt -CAkey ca.key -set_serial 01 -out server.crt 
+openssl x509 -req -passin pass:1111 -days 3650 -in server.csr -CA ca.crt -CAkey ca.key -set_serial 01 -out server.crt -extensions req_ext -extfile ssl.cnf
 
 # Step 5: Convert the server certificate to .pem format (server.pem) - usable by gRPC
 openssl pkcs8 -topk8 -nocrypt -passin pass:1111 -in server.key -out server.pem
