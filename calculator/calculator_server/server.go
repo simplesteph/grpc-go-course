30badcfb94d180534249f57d65e18bf5de5b88cc
Victor Gwee <47884004+vicgwee@users.noreply.github.com>
2022-02-05 07:18:23 +0800

    Unary API (#1)
    * init greet only
    
    * delete generated file and add to gitignore
    
    * change path in greet.proto
    
    * reduce to boilerplate code
    
    * Example: Greet RPC
    
    * delete generated files
    
    * add .pb.go files to gitignore
    
    * implement calculator sum

--- calculator/calculator_server/server.go
+++ calculator/calculator_server/server.go
@@ -3,16 +3,11 @@ package main
 import (
 	"context"
 	"fmt"
-	"io"
 	"log"
-	"math"
 	"net"
 
-	"google.golang.org/grpc/codes"
-	"google.golang.org/grpc/status"
-
-	"github.com/simplesteph/grpc-go-course/calculator/calculatorpb"
-	"google.golang.org/grpc/reflection"
+	"../calculatorpb"
+	"google.golang.org/grpc/credentials"
 
 	"google.golang.org/grpc"
 )
@@ -20,98 +15,13 @@ import (
 type server struct{}
 
 func (*server) Sum(ctx context.Context, req *calculatorpb.SumRequest) (*calculatorpb.SumResponse, error) {
-	fmt.Printf("Received Sum RPC: %v\n", req)
-	firstNumber := req.FirstNumber
-	secondNumber := req.SecondNumber
-	sum := firstNumber + secondNumber
+
+	result := req.GetFirstNumber() + req.GetSecondNumber()
+
 	res := &calculatorpb.SumResponse{
-		SumResult: sum,
+		SumResult: result,
 	}
 	return res, nil
-}
-
-func (*server) PrimeNumberDecomposition(req *calculatorpb.PrimeNumberDecompositionRequest, stream calculatorpb.CalculatorService_PrimeNumberDecompositionServer) error {
-	fmt.Printf("Received PrimeNumberDecomposition RPC: %v\n", req)
-
-	number := req.GetNumber()
-	divisor := int64(2)
-
-	for number > 1 {
-		if number%divisor == 0 {
-			stream.Send(&calculatorpb.PrimeNumberDecompositionResponse{
-				PrimeFactor: divisor,
-			})
-			number = number / divisor
-		} else {
-			divisor++
-			fmt.Printf("Divisor has increased to %v\n", divisor)
-		}
-	}
-	return nil
-}
-
-func (*server) ComputeAverage(stream calculatorpb.CalculatorService_ComputeAverageServer) error {
-	fmt.Printf("Received ComputeAverage RPC\n")
-
-	sum := int32(0)
-	count := 0
-
-	for {
-		req, err := stream.Recv()
-		if err == io.EOF {
-			average := float64(sum) / float64(count)
-			return stream.SendAndClose(&calculatorpb.ComputeAverageResponse{
-				Average: average,
-			})
-		}
-		if err != nil {
-			log.Fatalf("Error while reading client stream: %v", err)
-		}
-		sum += req.GetNumber()
-		count++
-	}
-
-}
-
-func (*server) FindMaximum(stream calculatorpb.CalculatorService_FindMaximumServer) error {
-	fmt.Println("Received FindMaximum RPC")
-	maximum := int32(0)
-
-	for {
-		req, err := stream.Recv()
-		if err == io.EOF {
-			return nil
-		}
-		if err != nil {
-			log.Fatalf("Error while reading client stream: %v", err)
-			return err
-		}
-		number := req.GetNumber()
-		if number > maximum {
-			maximum = number
-			sendErr := stream.Send(&calculatorpb.FindMaximumResponse{
-				Maximum: maximum,
-			})
-			if sendErr != nil {
-				log.Fatalf("Error while sending data to client: %v", sendErr)
-				return sendErr
-			}
-		}
-	}
-}
-
-func (*server) SquareRoot(ctx context.Context, req *calculatorpb.SquareRootRequest) (*calculatorpb.SquareRootResponse, error) {
-	fmt.Println("Received SquareRoot RPC")
-	number := req.GetNumber()
-	if number < 0 {
-		return nil, status.Errorf(
-			codes.InvalidArgument,
-			fmt.Sprintf("Received a negative number: %v", number),
-		)
-	}
-	return &calculatorpb.SquareRootResponse{
-		NumberRoot: math.Sqrt(float64(number)),
-	}, nil
 }
 
 func main() {
@@ -122,11 +32,21 @@ func main() {
 		log.Fatalf("Failed to listen: %v", err)
 	}
 
-	s := grpc.NewServer()
-	calculatorpb.RegisterCalculatorServiceServer(s, &server{})
+	opts := []grpc.ServerOption{}
+	tls := false
+	if tls {
+		certFile := "ssl/server.crt"
+		keyFile := "ssl/server.pem"
+		creds, sslErr := credentials.NewServerTLSFromFile(certFile, keyFile)
+		if sslErr != nil {
+			log.Fatalf("Failed loading certificates: %v", sslErr)
+			return
+		}
+		opts = append(opts, grpc.Creds(creds))
+	}
 
-	// Register reflection service on gRPC server.
-	reflection.Register(s)
+	s := grpc.NewServer(opts...)
+	calculatorpb.RegisterCalculatorServiceServer(s, &server{})
 
 	if err := s.Serve(lis); err != nil {
 		log.Fatalf("failed to serve: %v", err)
--- greet/greet_server/server.go
+++ greet/greet_server/server.go
@@ -3,106 +3,20 @@ package main
 import (
 	"context"
 	"fmt"
-	"io"
 	"log"
 	"net"
-	"strconv"
-	"time"
 
-	"google.golang.org/grpc/codes"
-	"google.golang.org/grpc/credentials"
-	"google.golang.org/grpc/status"
-
-	"github.com/simplesteph/grpc-go-course/greet/greetpb"
-
+	"../greetpb"
 	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials"
 )
 
 type server struct{}
 
 func (*server) Greet(ctx context.Context, req *greetpb.GreetRequest) (*greetpb.GreetResponse, error) {
-	fmt.Printf("Greet function was invoked with %v\n", req)
 	firstName := req.GetGreeting().GetFirstName()
 	result := "Hello " + firstName
 	res := &greetpb.GreetResponse{
-		Result: result,
-	}
-	return res, nil
-}
-
-func (*server) GreetManyTimes(req *greetpb.GreetManyTimesRequest, stream greetpb.GreetService_GreetManyTimesServer) error {
-	fmt.Printf("GreetManyTimes function was invoked with %v\n", req)
-	firstName := req.GetGreeting().GetFirstName()
-	for i := 0; i < 10; i++ {
-		result := "Hello " + firstName + " number " + strconv.Itoa(i)
-		res := &greetpb.GreetManytimesResponse{
-			Result: result,
-		}
-		stream.Send(res)
-		time.Sleep(1000 * time.Millisecond)
-	}
-	return nil
-}
-
-func (*server) LongGreet(stream greetpb.GreetService_LongGreetServer) error {
-	fmt.Printf("LongGreet function was invoked with a streaming request\n")
-	result := ""
-	for {
-		req, err := stream.Recv()
-		if err == io.EOF {
-			// we have finished reading the client stream
-			return stream.SendAndClose(&greetpb.LongGreetResponse{
-				Result: result,
-			})
-		}
-		if err != nil {
-			log.Fatalf("Error while reading client stream: %v", err)
-		}
-
-		firstName := req.GetGreeting().GetFirstName()
-		result += "Hello " + firstName + "! "
-	}
-}
-
-func (*server) GreetEveryone(stream greetpb.GreetService_GreetEveryoneServer) error {
-	fmt.Printf("GreetEveryone function was invoked with a streaming request\n")
-
-	for {
-		req, err := stream.Recv()
-		if err == io.EOF {
-			return nil
-		}
-		if err != nil {
-			log.Fatalf("Error while reading client stream: %v", err)
-			return err
-		}
-		firstName := req.GetGreeting().GetFirstName()
-		result := "Hello " + firstName + "! "
-
-		sendErr := stream.Send(&greetpb.GreetEveryoneResponse{
-			Result: result,
-		})
-		if sendErr != nil {
-			log.Fatalf("Error while sending data to client: %v", sendErr)
-			return sendErr
-		}
-	}
-
-}
-
-func (*server) GreetWithDeadline(ctx context.Context, req *greetpb.GreetWithDeadlineRequest) (*greetpb.GreetWithDeadlineResponse, error) {
-	fmt.Printf("GreetWithDeadline function was invoked with %v\n", req)
-	for i := 0; i < 3; i++ {
-		if ctx.Err() == context.DeadlineExceeded {
-			// the client canceled the request
-			fmt.Println("The client canceled the request!")
-			return nil, status.Error(codes.Canceled, "the client canceled the request")
-		}
-		time.Sleep(1 * time.Second)
-	}
-	firstName := req.GetGreeting().GetFirstName()
-	result := "Hello " + firstName
-	res := &greetpb.GreetWithDeadlineResponse{
 		Result: result,
 	}
 	return res, nil
